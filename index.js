45c53791c8922ee850e45800b9c2219a81ff3b02
Quinn Thibeault <quinn.thibeault96@gmail.com>
2017-02-28 23:05:20 -0700

    Add better handling

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const create = require('./lib/create')
 
 const app = program
     .version('0.0.1')
-    .command('create <name>')
+    .command('create <name>', { default: true });
     .action(function (name) {
         console.log('Creating a new migration configuration');
         create(name)
             .then(() => {
+                console.log(`Created configuration ${name} successfully;`)
                 process.exit(0);
+            })
+            .catch(function(err) {
+                console.error(`Could not create configuration ${name}`);
+                console.error(err);
+                process.exit(-1);
             });
     });
 
