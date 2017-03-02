ba8fbdca778a7076344f9547a2b55be0b5dd2411
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2017-03-02 13:26:16 -0700

    export services to make main file cleaner

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 'use strict';
 
 const program = require('commander');
-
-const create = require('./lib/create');
-const migrate = require('./lib/migrate');
+const services = require('./lib');
 
 const app = program
     .version('0.0.1')
     .command('create <name>', { default: true })
-    .action(function (name) {
-        console.log('Creating a new migration configuration');
-        create(name)
-            .then(() => {
-                console.log(`Created configuration ${name} successfully;`)
-                process.exit(0);
-            })
-            .catch(function(err) {
-                console.error(`Could not create configuration ${name}`);
-                console.error(err);
-                process.exit(-1);
-            });
-    })
+        .action(services.createMigration)
     .command('migrate <name>')
-    .action(function (name) {
-        console.log(`Running migration [${name}]`);
-        // TODO
-    });
+        .action(function (name) {
+            console.log(`Running migration [${name}]`);
+            // TODO
+        });
 
 module.exports = app;
